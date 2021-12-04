0449a529af6607859f5ea421c85042f0ec1d9d3b
Alvaro-UPM <alvaro.garcialop@alumnos.upm.es>
2021-12-04 12:28:57 -0500

    GFF 3 files and report included

--- Assignment3/Assignment3_MainCode.rb
+++ Assignment3/Assignment3_MainCode.rb
@@ -61,6 +61,10 @@ we are using this regular expression to discard these "complement" exons and foc
 
 cttctt_match = Regexp.new(/cttctt/i) #we use this regular expression to check if there is 'cttctt' in the seq string
 
+aagaag_match = Regexp.new(/aagaag/i) 
+#we use this regular expression to check if there is 5' 'aagaag' 3' in the forward sequence string, that is,
+# 5' 'cttctt' 3' in its reverse sequence
+
 chromosome = Regexp.new(/chromosome:TAIR10:([1-5]):(\d+):(\d+)/i) #we use this to get the chromosome coordinates of the genes
 # .............................
 
@@ -89,7 +93,7 @@ end
 
 
 
-# Adding new 'cttctt' features to the EnsEMBL Sequence object
+# Adding new 'cttctt' features to the EnsEMBL Sequence objects
 #.....................................
 all_embl.each do |embl|
 
@@ -109,10 +113,17 @@ all_embl.each do |embl|
            exon_seq[i] = seq["#{exon_pos[i][0]}".to_i.."#{exon_pos[i][1]}".to_i] # and get it sequence using the positions 
            #in the complete gene sequence (previously called "seq")
            if cttctt_match.match(exon_seq[i]) # if there is 'cttctt' inside the exon_seq, get its coordinates (using the method below)
-                cttctt_coordinates[j] = [exon_seq[i].enum_for(:scan, /cttctt/i).map { Regexp.last_match.begin(0) }[0] + exon_pos[i][0].to_i, exon_seq[i].enum_for(:scan, /cttctt/i).map { Regexp.last_match.end(0) }[0] + exon_pos[i][0].to_i - 1].uniq.compact 
+                cttctt_coordinates[j] = [exon_seq[i].enum_for(:scan, /cttctt/i).map { Regexp.last_match.begin(0) }[0] + exon_pos[i][0].to_i, exon_seq[i].enum_for(:scan, /cttctt/i).map { Regexp.last_match.end(0) }[0] + exon_pos[i][0].to_i - 1, "+"].uniq.compact 
                  #we substract -1 at the end (exon_pos[i][0].to_i - 1) in order to get the real ending position, that is, the last 'T' (and not like an ruby array numeration)
                 j=j+1
           end
+           
+           if aagaag_match.match(exon_seq[i]) # if there is 'aagaag' inside the exon_seq, get its coordinates (using the method below)
+                cttctt_coordinates[j] = [exon_seq[i].enum_for(:scan, /aagaag/i).map { Regexp.last_match.begin(0) }[0] + exon_pos[i][0].to_i, exon_seq[i].enum_for(:scan, /aagaag/i).map { Regexp.last_match.end(0) }[0] + exon_pos[i][0].to_i - 1, "-"].uniq.compact 
+                 #we substract -1 at the end (exon_pos[i][0].to_i - 1) in order to get the real ending position, that is, the last 'T' (and not like an ruby array numeration)
+                j=j+1
+          end
+           
           i=i+1
          end
       end
@@ -141,77 +152,68 @@ Here, we fix the "string" that we want to be always in some of these "fields".
 In fact, we are fixing the most of them and we are only having differences "string" in "seqid", "start" and "end" fiedls.
 =end
 
-source = "BioRuby"
+source = "EnsEMBL"
 type = "direct_repeat"
-strand = "."
 score = "."
 phase = "."
 attributes = "."
 #.....................................
 
 
-# First GFF3 file creation
+# Both first GFF3 file and no_cttctt_report.txt creation
 #.....................................
-gff3 = File.new("gff3_first", "w") # we open the gff3 file to write the CTTCTT features (called "gff3_first)
+gff3 = File.new("gff3_first.gff3", "w") # we open the gff3 file to write the CTTCTT features (called "gff3_first.gff3")
 gff3.write("##gff-version 3\n") # we write mandatory first line
-headers = ["seqid", "source", "type", "start", "end", "strand", "phase", "attributes"] 
-# Here above, we define the string that we are going to use as headers in the gff3 file.
-headers = headers.join("\t") # we join the headers by \t, because gff3 is a tab-separated format
-gff3.write(headers+"\n") # we write the headers
 
-report = File.new("report", "w") # we open another file (called "report") to report the genes (if any) 
+report = File.new("no_cttctt_report.txt", "w") # we open another file (called "no_cttctt_report.txt") to report the genes (if any) 
 #that have not any 'cttctt' repeat in their exons
-report.write("Here we report the genes that exons with CTTCTT repeats:\n") # we write the first line of this report
+report.write("Here we report the AGIs of the genes which exons have not any 'CTTCTT' repeats in the sequence:\n") # we write the first line of this report
 
 
 all_embl.each do |embl|
 
-  fields_arr = [] 
+fields_arr = [] 
 =begin
 Here above, we create a fields_arr in which we are going to introduce the values 
 of the gff3 fields for each 'cttctt' repeat of each embl. 
 Latter, we are going to use it to know if there is any 'cttctt' in that embl or not to make the report.
 =end
-  seqid = []
-=begin
-We define the seqid as an array since there are more than one "gene" feature in some web response, 
-with different AGIs differing by 10 units in their AGI number, e.g., "at5g542702 and "at5g54280"
-(I do not really know why).
-Although it could be wrong, we are going to consider this AGIs as the same and we are going to pick up the first one o
-of the "seqid" array as the seqid
-=end
+  
+  seqid = "" # defining seqid by default as a "empty" string to prevent from problems
   
   j = 0  # parameters just for the mentioned arrays elements itinerations (respectively)
-  id = 0
+  differentiator = 1
 
-  embl.features.each do |feature|
+ embl.features.each do |feature|
 
       if feature.feature == "gene" # if feature is called "gene"
-        seqid[id] = "#{feature.qualifiers[0].value}" #we get the AGI as "seqid" 
-        id = id+1
+        seqid = "#{feature.qualifiers[0].value}" #we get the AGI as "seqid" 
       end
 
       next unless feature.feature == "cttctt" # pass to the next feature unless the feature is called "cttctt" (our created feature)
         feature.position.each do |pos| # each one do pos (each pos is an array with 2 elements: start and end positions, respectively)
+          seqid = "#{seqid.split(".")[0]}" + ".#{differentiator}"
           start_pos = "#{pos[0]}".to_s # get the start_pos as the first elemet of pos array
           end_pos = "#{pos[1]}".to_s # get the end_pos as the second elemet of pos array
-          fields_arr[j] = [seqid[0], source, type, start_pos, end_pos, score, strand, phase, attributes] 
+          strand = "#{pos[2]}".to_s # get if the strand is positive or negative
+          fields_arr[j] = [seqid, source, type, start_pos, end_pos, score, strand, phase, attributes] 
           # above introduce a new elements in the "fields_arr" array
           new_line = fields_arr[j].join("\t") + "\n" #we join these new elements (adding \n for new line)
           gff3.write(new_line) # an we write them as a new line in the gff3 file
           j=j+1
+          differentiator = differentiator + 1
       end
     
   end
   if fields_arr == [] #if the fields_arr keeps empty after all the features itinerations 
   # that means that it was not created a new feature called "cttctt" for that embl
   # because there is not 'cttct' repeats in any of its exons' sequences
-    report.write("#{seqid[0]}\n") # So, in this case, we write its seqid as a new line in the report file
+    report.write("#{seqid}\n") # So, in this case, we write its seqid as a new line in the report file
   end
     
 end
-gff3.close # file called "gff3_first"
-report.close # file called "report"
+gff3.close # file called "gff3_first.gff3"
+report.close # file called "no_cttctt_report.txt"
 # we close both files
 #.....................................
 
@@ -220,18 +222,16 @@ report.close # file called "report"
 # Second GFF3 file creation
 #.....................................
 
-gff3_chr = File.new("gff3_chr", "w") # we open the gff3 file to write the CTTCTT features
+gff3_chr = File.new("gff3_chr.gff3", "w") # Now, we open the gff3 file to write the CTTCTT features (called "gff3_first.gff3"),
+#but using the chromosome coordinates instead of the genes's ones
 gff3_chr.write("##gff-version 3\n") # we write mandatory first line
-headers = ["seqid", "source", "type", "start", "end", "strand", "phase", "attributes"] 
-# Here above, we define the string that we are going to use as headers in the gff3 file.
-headers = headers.join("\t") # we join the headers by \t, because gff3 is a tab-separated format
-gff3_chr.write(headers+"\n") # we write the headers
 
-
-chr = []
+chr = [] # empty arrays to introduce the chromosome number,
+# start and en dposition of the gene in the chromosome
 chr_start = []
 chr_end = []
 k=0 #itinerator
+
 all_record.each do |record|
   if record.match(chromosome) # if record match "chromosome" RegExp (defined above in "Regular Expression")
     chr[k] = record.match(chromosome)[1] # get the chromosome number as chr
@@ -252,7 +252,7 @@ as the new start and end positions in our new gff3 file called "gff3_chr"
 k=0 # we use the same letter for the itinerator to make clear the pacing between "chr", "chr_start" and "all_embl" itinerations
 all_embl.each do |embl|
 
-  seqid = "Chr#{chr[k]}"
+  seqid = "chr#{chr[k]}"
 
   embl.features.each do |feature|
 
@@ -260,6 +260,7 @@ all_embl.each do |embl|
         feature.position.each do |pos| # each one do pos (each pos is an array with 2 elements: start and end positions, respectively)
           start_pos = "#{pos[0].to_i + chr_start[k].to_i - 1}".to_s # get the start_pos as the first elemet of pos array
           end_pos = "#{pos[1].to_i + chr_start[k].to_i - 1}".to_s # get the end_pos as the second elemet of pos array
+          strand = "#{pos[2]}".to_s # get if the strand is positive or negative
           fields = [seqid, source, type, start_pos, end_pos, score, strand, phase, attributes] 
           # above introduce a new elements in the "fields_arr" array
           new_line = fields.join("\t") + "\n" #we join these new elements (adding \n for new line)
@@ -269,6 +270,6 @@ all_embl.each do |embl|
   end
 k=k+1 # again, we always increase the value in each iteration because want to keep the pacing
 end
-gff3_chr.close # file called "gff3_chr"
+gff3_chr.close # file called "gff3_chr.gff3"
 # we close the file 
 #.....................................
