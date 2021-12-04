64e70374ee117af5a06e6f45c3df42009ef5a2a3
Alvaro-UPM <alvaro.garcialop@alumnos.upm.es>
2021-12-04 08:33:06 -0500

    GFF 3 files and report included

--- Assignment3/Assignment3_MainCode.rb
+++ Assignment3/Assignment3_MainCode.rb
@@ -5,7 +5,7 @@ require 'enumerator' #to get the coordinates of the match
 
 
 # Create a function called "fetch" that we can re-use everywhere in our code
-
+#.....................................
 def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
   response = RestClient::Request.execute({
     method: :get,
@@ -28,9 +28,11 @@ def fetch(url, headers = {accept: "*/*"}, user = "", pass="")
     response = false
     return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
 end
+#.....................................
 
 
-
+# Getting AGIs from the 168 AGIs' list
+#.....................................
 =begin
 Here, we create a has with the "clean" AGIs from our original list.
 =end
@@ -44,7 +46,7 @@ original_list_lines.each do |x|
   original_agi[i] = original_list_lines[i].partition("\n")[0].downcase
   i=i+1
 end
-
+#.....................................
 
 
 # Regular expressions
@@ -58,18 +60,24 @@ we are using this regular expression to discard these "complement" exons and foc
 =end
 
 cttctt_match = Regexp.new(/cttctt/i) #we use this regular expression to check if there is 'cttctt' in the seq string
+
+chromosome = Regexp.new(/chromosome:TAIR10:([1-5]):(\d+):(\d+)/i) #we use this to get the chromosome coordinates of the genes
 # .............................
 
 
 
-i=0 # just to itinerate into the "all_embl" array's elements
+# Web access to retrieve the data and introduce it in Bio:EMBL object
+#.....................................
+i=0 # just to itinerate into the "all_record" and all_embl" arrays elements at the same time
+all_record = [] # an array to get all the "dirty" response body
 all_embl = [] # an array in which we are going to introduce all the Bio:EMBL created from the web call response of all the AGIs
 original_agi.each do |x|
   
   res = fetch("http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&format=embl&id=#{x}"); #calling the web
 
   if res 
-    record = res.body  
+    record = res.body
+    all_record[i] = res.body 
     all_embl[i] = Bio::EMBL.new(res.body) # introducing the body's response into a new Bio:EMBL (EnsEMBL Sequence Object)
     # and, each of these ones, into "all_embl" array
     i=i+1
@@ -77,9 +85,12 @@ original_agi.each do |x|
     puts "the Web call failed - see STDERR for details..."
   end
 end
+#.....................................
 
 
 
+# Adding new 'cttctt' features to the EnsEMBL Sequence object
+#.....................................
 all_embl.each do |embl|
 
   seq = embl.seq # seq is the sequence of the embl
@@ -119,9 +130,11 @@ And we add this new Feature to our EnsEMBL Sequence Object (embl)
   embl.features
   
 end
+#.....................................
 
 
-
+# Defining common fields
+#.....................................
 =begin
 Since the gff3 format "fields" (or headers) are: "seqid", "source", "type", "start", "end", "strand", "phase", "attributes"
 Here, we fix the "string" that we want to be always in some of these "fields".
@@ -134,18 +147,19 @@ strand = "."
 score = "."
 phase = "."
 attributes = "."
+#.....................................
 
 
-
-
-gff3 = File.new("gff3", "w") # we open the gff3 file to write the CTTCTT features
+# First GFF3 file creation
+#.....................................
+gff3 = File.new("gff3_first", "w") # we open the gff3 file to write the CTTCTT features (called "gff3_first)
 gff3.write("##gff-version 3\n") # we write mandatory first line
 headers = ["seqid", "source", "type", "start", "end", "strand", "phase", "attributes"] 
 # Here above, we define the string that we are going to use as headers in the gff3 file.
 headers = headers.join("\t") # we join the headers by \t, because gff3 is a tab-separated format
 gff3.write(headers+"\n") # we write the headers
 
-report = File.new("report", "w") # we open another file to report the genes (if any) 
+report = File.new("report", "w") # we open another file (called "report") to report the genes (if any) 
 #that have not any 'cttctt' repeat in their exons
 report.write("Here we report the genes that exons with CTTCTT repeats:\n") # we write the first line of this report
 
@@ -196,5 +210,65 @@ of the "seqid" array as the seqid
   end
     
 end
-gff3.close
-report.close # we close both files
+gff3.close # file called "gff3_first"
+report.close # file called "report"
+# we close both files
+#.....................................
+
+
+
+# Second GFF3 file creation
+#.....................................
+
+gff3_chr = File.new("gff3_chr", "w") # we open the gff3 file to write the CTTCTT features
+gff3_chr.write("##gff-version 3\n") # we write mandatory first line
+headers = ["seqid", "source", "type", "start", "end", "strand", "phase", "attributes"] 
+# Here above, we define the string that we are going to use as headers in the gff3 file.
+headers = headers.join("\t") # we join the headers by \t, because gff3 is a tab-separated format
+gff3_chr.write(headers+"\n") # we write the headers
+
+
+chr = []
+chr_start = []
+chr_end = []
+k=0 #itinerator
+all_record.each do |record|
+  if record.match(chromosome) # if record match "chromosome" RegExp (defined above in "Regular Expression")
+    chr[k] = record.match(chromosome)[1] # get the chromosome number as chr
+    chr_start[k] = record.match(chromosome)[2] # get the start coordinate of the gene inside the chromosome
+    chr_end[k] = record.match(chromosome)[3] #just in case
+  end
+  k=k+1 # we increase the value of the itinerator even if there is not match 
+  #because we want to keep it in the same order as "all_record" and "all_embl" arrays
+end
+
+=begin
+Since "all_record" and "all_embl" contains the web call body response in the same order 
+(they were created using the same itenrator), we can itinerate in both at the same time to 
+include the chromosome's number as the new seqid and the 'cttctt' repeat chromosome's coordinates 
+as the new start and end positions in our new gff3 file called "gff3_chr"
+=end
+
+k=0 # we use the same letter for the itinerator to make clear the pacing between "chr", "chr_start" and "all_embl" itinerations
+all_embl.each do |embl|
+
+  seqid = "Chr#{chr[k]}"
+
+  embl.features.each do |feature|
+
+      next unless feature.feature == "cttctt" # pass to the next feature unless the feature is called "cttctt" (our created feature)
+        feature.position.each do |pos| # each one do pos (each pos is an array with 2 elements: start and end positions, respectively)
+          start_pos = "#{pos[0].to_i + chr_start[k].to_i - 1}".to_s # get the start_pos as the first elemet of pos array
+          end_pos = "#{pos[1].to_i + chr_start[k].to_i - 1}".to_s # get the end_pos as the second elemet of pos array
+          fields = [seqid, source, type, start_pos, end_pos, score, strand, phase, attributes] 
+          # above introduce a new elements in the "fields_arr" array
+          new_line = fields.join("\t") + "\n" #we join these new elements (adding \n for new line)
+          gff3_chr.write(new_line) # an we write them as a new line in the gff3 file
+      end
+    
+  end
+k=k+1 # again, we always increase the value in each iteration because want to keep the pacing
+end
+gff3_chr.close # file called "gff3_chr"
+# we close the file 
+#.....................................
